03da7e4f9248dc2ab00bd7a11cffa82e58255202
Akif Manzoor <akif.manzoor@achievers.com>
2022-11-08 20:31:45 -0500

    updates request query and printed task names

--- import_tasks.py
+++ import_tasks.py
@@ -10,40 +10,29 @@ lists = {
 
 url = "https://api.clickup.com/api/v2/list/" + list_id + "/task"
 
-query = {
-  "archived": "false",
-  "page": "0",
-  "order_by": "string",
-  "reverse": "true",
-  "subtasks": "false",
-  "statuses": "string",
-  "include_closed": "true",
-  "assignees": "string",
-  "tags": "string",
-  "due_date_gt": "0",
-  "due_date_lt": "0",
-  "date_created_gt": "0",
-  "date_created_lt": "0",
-  "date_updated_gt": "0",
-  "date_updated_lt": "0",
-  "custom_fields": "string"
-}
-
-# create a secret env file to store the api key
 headers = {
   "Content-Type": "application/json",
   "Authorization": "pk_14759065_KDKZBH5ECUN3UZYYE00QY4MN7FLJ7GD1"
 }
 
-response = requests.get(url, headers=headers, params=query)
+query = {
+  "archived": "false",
+  "page": "0",
+  "reverse": "true",
+  "subtasks": "false",
+  "include_closed": "false",
+  "statuses": ['Open', 'In Progress', 'scheduled'],
+}
 
-data = response.json()
-print(data)
+# create a secret env file to store the api key
 
 timeframe = "2 weeks"
 schedule_by = "priority"
 
 
+
+def get_tasks():
+    return
 
 def filter_tasks():
     return
@@ -51,9 +40,12 @@ def filter_tasks():
 def decipher_avaliability():
     return
 
-#only retrieve tasks which have a time estimate
-def get_tasks():
-    return
-
 def schedule_tasks(timeframe, schedule_by):
     return
+
+
+response = requests.get(url, headers=headers, params=query)
+data = response.json()
+
+for task in data["tasks"]:
+    print(task["name"])
